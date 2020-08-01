f62a9ffce7911d9c37cfdad6594cf5d4755a1202
PeronTheDuck <pedroignacioperezvargas@gmail.com>
2020-07-31 23:01:49 -0300

    Modernize project

--- src/main.rs
+++ src/main.rs
@@ -1,131 +1,177 @@
 extern crate image;
+extern crate structopt;
+#[macro_use]
+extern crate log;
 
 use image::GenericImageView;
+use std::path::PathBuf;
+use structopt::StructOpt;
 
-fn main() {
-    let mut sector_size = 5;
-    let mut radius = (sector_size as f64) / 2f64;
-    let mut verbose = false;
-    let mut quad = false;
+// Index of each color
+const RED: usize = 0;
+const GREEN: usize = 1;
+const BLUE: usize = 2;
 
-    let mut input_file_name = String::from("input.png");
-    let mut output_file_name = String::from("output.png");
-    //#region Argument handling
-    let mut args_list: Vec<_> = std::env::args().rev().collect();
-    args_list.pop();
-    while args_list.len() > 0 {
-        let arg: String = args_list.pop().unwrap();
+struct Point {
+    x: f64,
+    y: f64,
+}
+impl Point {
+    /// Calculate the distance between this an another point
+    fn distance(&self, p: &Point) -> f64 {
+        let a = (self.x - p.x).powi(2);
+        let b = (self.y - p.y).powi(2);
+        let c = (a + b).sqrt();
 
-        match arg.as_ref() {
-            "--in" => {
-                input_file_name = args_list.pop().expect("No more arguments passed");
-            }
-            "--out" => {
-                output_file_name = args_list.pop().expect("No more arguments passed");
-            }
-            "--size" => {
-                sector_size = args_list
-                    .pop()
-                    .expect("No more arguments passed")
-                    .parse::<u32>()
-                    .expect("Couldn't parse input. It must be an u8");;
-            }
-            "--rad" => {
-                radius = args_list
-                    .pop()
-                    .expect("No more arguments passed")
-                    .parse::<f64>()
-                    .expect("Couldn't parse input. It must be an f64");;
-            }
-            "--verbose" => {
-                verbose = args_list
-                    .pop()
-                    .expect("No more arguments passed")
-                    .parse::<bool>()
-                    .expect("Couldn't parse input. It must be a bool");
-            }
-            "--quad" => {
-                quad = match args_list.pop() {
-                    Some(e) => e
-                        .parse::<bool>()
-                        .expect("Couldn't parse input. It must be a bool"),
-                    None => true,
-                }
-            }
-            &_ => {
-                println!("Meaningless {}", arg);
-            }
+        c
+    }
+}
+// Convert any tuple into a Point
+impl<N: Into<f64>> std::convert::From<(N, N)> for Point {
+    fn from(p: (N, N)) -> Self {
+        Self {
+            x: p.0.into(),
+            y: p.1.into(),
         }
     }
-    //#endregion
-    let img = image::open(&input_file_name).expect("Couldn't open file");
+}
+
+/// The kind of shape to draw
+#[derive(Debug, PartialEq)]
+enum Mode {
+    Quad,
+    Circle,
+}
+// Required for structopt
+impl std::str::FromStr for Mode {
+    type Err = &'static str;
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            "Quad" | "quad" => Ok(Self::Quad),
+            "Circle" | "circle" => Ok(Self::Circle),
+            _ => Err("Couldn't convert to Mode"),
+        }
+    }
+}
+
+#[derive(StructOpt, Debug)]
+#[structopt(name = "Color circler")]
+struct Args {
+    #[structopt(short, long, parse(from_os_str), help = "File to read")]
+    input: std::path::PathBuf,
+
+    #[structopt(
+        short,
+        long,
+        parse(from_os_str),
+        help = "File to write. If empty, uses 'output-[input].png'"
+    )]
+    output: Option<std::path::PathBuf>,
+
+    #[structopt(long, default_value = "Circle", help = "Kind of shape to use")]
+    mode: Mode,
+
+    /// Size of the shape
+    #[structopt(long, default_value = "5", help = "Size of the shape (diameter)")]
+    size: u32,
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    // Initialize program
+    env_logger::init();
+    let cfg: Args = Args::from_args();
+
+    let img = image::open(&cfg.input).expect("Couldn't open file");
     let (width, height) = (img.width(), img.height());
 
-    println!(
-        "{} is {}x{}. Processing with {}/{} as size/rad. Using {}",
-        &input_file_name, width, height, sector_size, radius,(match quad{true=>"quads",false=>"circles"})
+    info!(
+        "{:?} is {}x{}. Processing with {} as size. Using {:?}",
+        &cfg.input.to_str(),
+        width,
+        height,
+        cfg.size,
+        cfg.mode
     );
 
+    // Store each sector's color
     let mut map: Vec<[u32; 3]> = vec![];
-    let mut act_prom: [u32; 3] = [0, 0, 0];
 
-    //#region Processing
-    for y in (0..height).step_by(sector_size as usize) {
-        if (y + sector_size) > height {
+    // "Divide" the picture into sectors and iterate through each of them
+    for y in (0..height).step_by(cfg.size as usize) {
+        // Break if we get outside of the image
+        if (y + cfg.size) > height {
             break;
         }
-        for x in (0..width).step_by(sector_size as usize) {
-            if (x + sector_size) > width {
+        for x in (0..width).step_by(cfg.size as usize) {
+            // Break if we get outside of the image
+            if (x + cfg.size) > width {
                 break;
             }
 
-            for s_y in y..(y + sector_size) {
-                for s_x in x..(x + sector_size) {
-                    for i in 0..3 {
-                        act_prom[i] = act_prom[i] + img.get_pixel(s_x, s_y)[i] as u32;
-                    }
+            // Average color of this sector
+            let mut avg: [u32; 3] = [0, 0, 0];
+
+            // Generate the color of this sector
+            for s_y in 0..cfg.size {
+                for s_x in 0..cfg.size {
+                    let pixel = img.get_pixel(x + s_x, y + s_y);
+                    avg[RED] += pixel[RED] as u32;
+                    avg[GREEN] += pixel[GREEN] as u32;
+                    avg[BLUE] += pixel[BLUE] as u32;
                 }
             }
-            for i in 0..3 {
-                act_prom[i] = act_prom[i] / (sector_size * sector_size);
-            }
-            map.push(act_prom);
-            act_prom = [0, 0, 0];
+            avg[RED] /= cfg.size.pow(2);
+            avg[GREEN] /= cfg.size.pow(2);
+            avg[BLUE] /= cfg.size.pow(2);
+            map.push(avg);
         }
-        if verbose {
-            println!("{} rows of {} rows", height, y);
-        }
+        debug!("{} rows of {} rows", height, y);
     }
-    println!("Finished processing image. {} sectors", map.len());
-    //#endregion
+    info!("Finished processing image. {} sectors", map.len());
 
-    //#region Calculating pixels
-    let distance_calc = |point: [i32; 2], sector_center: [i32; 2]| {
-        let dx: u32 = (point[0] - sector_center[0]).abs() as u32;
-        let dy: u32 = (point[1] - sector_center[1]).abs() as u32;
-        let distance: f64 = ((dx.pow(2) + dy.pow(2)) as f64).sqrt();
-        distance
-    };
-
-    let img = image::RgbImage::from_fn(width, height, |x, y| {
-        let sector_index = (x / sector_size) + (width / sector_size) * (y / sector_size);
+    let img = image::RgbaImage::from_fn(width, height, |x, y| {
+        let sector_index = (x / cfg.size) + (width / cfg.size) * (y / cfg.size);
         let sector_index = sector_index as usize;
-        let sector_center: [i32; 2] = [
-            (x - x % sector_size + sector_size / 2) as i32,
-            (y - y % sector_size + sector_size / 2) as i32,
-        ];
+        let sector_center: Point = (
+            (x - x % cfg.size + cfg.size / 2),
+            (y - y % cfg.size + cfg.size / 2),
+        )
+            .into();
 
-        if (distance_calc(sector_center, [x as i32, y as i32]) > radius)&&!quad {
-            return image::Rgb([0xFF, 0xFF, 0xFF]);
+        // In circle mode we have to
+        // check the distance to the center of this circle
+        // if we are too far away then we just draw transparent
+        if cfg.mode == Mode::Circle && sector_center.distance(&(x, y).into()) > cfg.size as f64 / 2.
+        {
+            return image::Rgba([0xFF, 0xFF, 0xFF, 0x00]);
         }
-        image::Rgb([
-            map[sector_index][0] as u8,
-            map[sector_index][1] as u8,
-            map[sector_index][2] as u8,
+        image::Rgba([
+            map[sector_index][RED] as u8,
+            map[sector_index][GREEN] as u8,
+            map[sector_index][BLUE] as u8,
+            0xFF,
         ])
     });
-    //#endregion
 
-    println!("Saving image to {}", output_file_name);
-    img.save(output_file_name).expect("Couldn't save file");
+    // If we didn't receive an --output, then we have to create the output file using the input as reference
+    let output: PathBuf = match cfg.output {
+        Some(v) => v,
+        None => {
+            let mut output = cfg.input.clone();
+            let filename = cfg
+                .input
+                .file_name()
+                .expect("Input has no filename")
+                .to_str()
+                .expect("Filename is not valid UTF-8");
+            let filename = format!("output-{}", filename);
+            output.set_file_name(filename);
+            output.set_extension("png");
+            output
+        }
+    };
+    info!("Saving image to {:#?}", &output);
+    img.save(output).expect("Couldn't save file");
+
+    Ok(())
 }
